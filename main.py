480a70b1df9e44f9808bf47e940f3676b54c7dff
John Adams Jr <john.c.adams32@gmail.com>
2022-01-02 23:04:47 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,2 @@
+# Today's project with 100 days of code, is a quiz game. Built using object-oriented programing, I am tasked to create a
+# game using my own created classes. Poke around and play with the game a bit!
