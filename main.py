000e507418c6606d3d707a867122744af6a7d8c2
John Adams Jr <john.c.adams32@gmail.com>
2022-01-03 18:46:11 -0700

    Implemented correct question number, created object to start quiz

--- main.py
+++ main.py
@@ -3,9 +3,13 @@
 
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
 for item in question_data:
     new_question = Question(item["text"], item["answer"])
     question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
