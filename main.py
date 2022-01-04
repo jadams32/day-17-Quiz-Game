db360ee4a9d58c364413633d271ffc3e4bcd4a80
John Adams Jr <john.c.adams32@gmail.com>
2022-01-03 17:24:34 -0700

    List of questions, imports, and question class created

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 # Today's project with 100 days of code, is a quiz game. Built using object-oriented programing, I am tasked to create a
 # game using my own created classes. Poke around and play with the game a bit!
+
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+for item in question_data:
+    new_question = Question(item["text"], item["answer"])
+    question_bank.append(new_question)
