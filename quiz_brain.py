cf70b40b91def18728016b15c21df11e153bbf21
John Adams Jr <john.c.adams32@gmail.com>
2022-01-03 19:38:51 -0700

    Implemented checking answer function and score attribute

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,13 +1,17 @@
 
 class QuizBrain:
+
     def __init__(self, question_list):
+        self.score = 0
         self.question_number = 0
         self.question_list = question_list
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q{self.question_number}. {current_question.text}(True/False):")
+        user_answer = input(f"Q{self.question_number}. {current_question.text}(True/False):")
+        self.check_answer(user_answer, current_question.answer)
+
 
     def still_have_questions(self):
         """Returns True if there are questions left to answer, false otherwise."""
@@ -15,3 +19,13 @@ class QuizBrain:
             return False
         else:
             return True
+
+    def check_answer(self, user_answer, question_answer):
+        if user_answer == question_answer:
+            self.score += 1
+            print("You got it right!")
+        else:
+            print("You got it wrong!")
+        print(f"The correct answer was: {question_answer}")
+        print(f"Your current score is: {self.score}/{self.question_number}\n")
+
